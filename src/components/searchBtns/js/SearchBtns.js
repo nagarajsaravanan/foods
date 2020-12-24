bdfc13fc3ced3233a2097f9208b104115dda76da
NagarajR_Contus <nagaraj.r@contus.in>
2020-12-24 12:08:19 +0530

    get dynamic food items

--- src/components/searchBtns/js/SearchBtns.js
+++ src/components/searchBtns/js/SearchBtns.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../css/searchBtns.css'
 
 function SearchBtns(props) {
-    console.log('props', props);
+    const getItems = async () => {
+        console.log('get items');
+    }
     return (
         <div className="search-btn-container">
             {props && props.foodNames && props.foodNames.map((f) => (
-                <div className="search-btns">
-                <div>{f}</div>
+                <div id={`name-${f}`} key={f} onClick={() => props.item(f)} className="search-btns">
+                <div>{f.charAt(0).toUpperCase() + f.slice(1)}</div>
             </div>
             ))}
         </div>
--- src/pages/Home/js/Home.js
+++ src/pages/Home/js/Home.js
@@ -5,23 +5,38 @@ import SearchBtns from '../../../components/searchBtns/js/SearchBtns';
 
 function Home() {
     const [image, setImage] = useState(false);
-    let footNames = ['pizza','briyani'];
+    const [name, setName] = useState(null);
+    let foodNames = ['pizza','biryani', 'burger', 'dosa', 'idly'];
     useEffect(async () => {
+        setName(null);
+        getFoodImage();
+    }, [])
+
+    const getItem = (name) => {
+        setName(name);
+        getFoodImage(name);
+        // console.log('name',name);
+    }
+
+    const getFoodImage = async(name = false) => {
         setImage(false);
-        let res = await fetch('https://foodish-api.herokuapp.com/api/');
+        let url = '';
+        url = name ? `https://foodish-api.herokuapp.com/api/images/${name}`:'https://foodish-api.herokuapp.com/api/';
+        let res = await fetch(url);
         res = await res.json();
         setImage((await res && await res.image) ? res.image : false);
-    }, [])
+    }
     return (
         <div className="home">
             <div className="food-container">
                 {image && image !== false ? <div className="food-pic">
-                    <img src={image} title="image" />
+                {name && name!= null && <div className="food-name">{name.charAt(0).toUpperCase() + name.slice(1)}</div>}
+                    <img src={image} title="" />
                 </div> : <div className="food-pic default-img">
-                        <img src={DefaultImg} title="image" />
+                        <img src={DefaultImg} title="Foodies!!" />
                     </div>}
             </div>
-            <SearchBtns foodNames={footNames}/>
+            <SearchBtns foodNames={foodNames} item={getItem}/>
         </div>
     );
 }
